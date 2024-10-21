b4e41991fd9f813c6ffb16b31bcc9fd8a860fb39
AshtonRopp <ashton.ropp@gmail.com>
2024-10-20 20:09:53 -0400

    Began writing README.md and compiling experimental results

--- SSD-Profiling/README.md
+++ SSD-Profiling/README.md
@@ -2,15 +2,36 @@
 
 ## Project Highlights
 
-- Highlight 1
+- Optimized data access operations using access size and queueing length
+- Developed Perl script for experiment orchestration
+- Collected performance data using FIO tool
+- Deployed Python script for graphing
 
 ## Overview
+This project utilizes the FIO tool to test SSD latency and queue length. It will discuss the performance implications of data access size, read/write intensity, and I/O queue length. I will also attempt to maximize my SSD's performance and compare it to a standard server SSD.
 
-Add overview
+## FIO Tool Background
+FIO (Flexible I/O Tester) is a popular benchmarking tool used to measure and simulate I/O performance on storage devices and systems. It has the following key features: 
+- **I/O Pattern Configuration**: Supports sequential, random, read, write, and mixed workloads.
+- **Customizable**: Highly configurable with options for block size, I/O depth, threads, and more.
+- **Cross-Platform Compatibility**: Available on Linux, Windows, WSL, and other systems.
+- **Output**: Provides detailed performance metrics like throughput, latency, and IOPS.
 
-## Implementation Details
+For more details, visit the [FIO GitHub repository](https://github.com/axboe/fio).
 
-Add testing information
+## Key FIO Command Line Options for this Project
+- `--bs=<int>`: sets the block size for each I/O operation during testing
+- `--rw=<operation>`: sets the read/write operation. Let operation = `randread` or `randwrite` for random testing
+- `--iodepth=<int>`: sets the maximum number of requests that can be queued
+- `--size=<int>`: sets the size limit. You can use `GiB`, `MiB`, or `KiB` instead of 1024^x
+
+## Data Access Size
+
+## Read/Write Intensity
+
+## I/O Queue Length
+
+## Comparison to Intel Data Center NVME SSD D7-P5600
 
 ## Getting Started
 
@@ -19,13 +40,33 @@ Add testing information
    git clone https://github.com/AshtonRopp/AdvancedComputerSystems.git
    cd SSD-Profiling
    ```
+2. Install dependencies:
+   - JSON (Perl)
+   - Text::CSV (Perl)
+   - Pandas (Python)
+   - Matplotlib (Python)
+3. Edit test.pl to suit the runtime environment:
+   - Set `$write_path` and `$read_path` to the desired locations
+   - Edit the line `system("python3 graph.py $graphCMD");` to use a Python version which has Matplotlib
+4. Allocate read/write storage for the experiment:
+   ```
+   perl test.p1 setup
+   ```
+5. Run the executable:
+   ```
+   perl test.pl run [option] [number of runs (optional)]
+   ```
+   Options:
+   - `data_access`: Test various data access sizes
+   - `rw_cent`: Test various read read/write ratios
+   - `queue_depth`: Test various queue depths
 
-2. Ensure you have the necessary dependencies:
-- FIO CLI capabilities
-
-## Contributors
-
-Ashton Ropp
-
-TODO:
--Mention topics covered here if used in final copy: https://unix.stackexchange.com/questions/459045/what-exactly-is-iodepth-in-fio
+   Example:
+   ```
+   ./perl test.pl run data_access
+   ```
+6. View the graphed data in plot.png
+7. Remove SSD partitions after all testing is finished:
+   ```
+   perl test.pl clean
+   ```
--- SSD-Profiling/test.pl
+++ SSD-Profiling/test.pl
@@ -6,28 +6,27 @@ use Text::CSV;
 
 my $task = $ARGV[0];
 my $experiment = $ARGV[1];
+my $input_runs = $ARGV[2];
 
+# Edit for your system:
 my $write_path = '/home/ashton/tmp/cache_test_write';
-my $write_path2 = '/home/ashton/tmp/cache_test_write2';
-
 my $read_path = '/home/ashton/tmp/cache_test_read';
-my $read_path2 = '/home/ashton/tmp/cache_test_read2';
-
+# End editing
 
 if (scalar @ARGV == 0) {
-    print("No input provided \n");
-    print("Please select \"setup\", \"run\", or \"clean\"\n");
-    exit();
+    die "Please select \"setup\", \"run\", or \"clean\"\n";
 }
 
-chomp($task);
-chomp($experiment);
+foreach my $arg (@ARGV) {
+    if (defined $arg && $arg ne '') {
+        chomp($arg) if $arg =~ /\R$/;
+    }
+}
 
 my $n = 2; # Number of GiB
 my $full_size = $n*1024*1024*1024; # n GiB
 my $half_size = $full_size/2;      # n/2 GiB
 my $count = $full_size/1024/1024; # Number of MiB in full_size
-
 
 if ($task eq 'setup') {
     # MiB = 1024*1024 B
@@ -45,31 +44,37 @@ elsif ($task eq 'run') {
     my $graphCMD;    # Type of graph to create
 
     if ($experiment eq 'data_access') {
-        my $num_runs = 5;  # Number of iterations to run each test
+        my $num_runs = 40;  # Number of iterations to run each test
+
+        # Use input number of runs if specified
+        if(@ARGV == 3) {
+            $num_runs = $input_runs;
+        }
+
         $graphCMD = 'plot_both';
         $x_column = 'Access Size';
-        @x_data = (8*1024, 16*1024, 32*1024, 64*1024, 128*1024);
+        @x_data = (16*1024, 32*1024, 64*1024, 128*1024);
 
         for my $i (0 .. $#x_data) {
             my ($read_lat_sum, $read_speed_sum, $write_lat_sum, $write_speed_sum) = (0, 0, 0, 0);
 
             for (1 .. $num_runs) {  # Run the test $num_runs times
                 # Run read test
-                system("fio test.fio --output-format=json --output=output.json --filename=$read_path --rw=randread --size=$full_size --bs=$x_data[$i] --iodepth=16");
+                system("fio test.fio --output-format=json --output=output.json --filename=$read_path --rw=randread --size=$full_size --bs=$x_data[$i] --iodepth=32 --runtime=3 --time_based");
                 my $data = decode_json_file('output.json');
 
                 foreach my $job (@{$data->{'jobs'}}) {
                     $read_lat_sum += $job->{'read'}->{'lat_ns'}->{'mean'};
-                    $read_speed_sum += $job->{'read'}->{'io_bytes'} / $job->{'read'}->{'runtime'} / 1024 / 1024 * 1000;
+                    $read_speed_sum += $job->{'read'}->{'bw_mean'} / 1024;
                 }
 
                 # Run write test
-                system("fio test.fio --output-format=json --output=output.json --filename=$write_path --rw=randwrite --size=$full_size --bs=$x_data[$i] --iodepth=16");
+                system("fio test.fio --output-format=json --output=output.json --filename=$write_path --rw=randwrite --size=$full_size --bs=$x_data[$i] --iodepth=32 --runtime=3 --time_based");
                 $data = decode_json_file('output.json');
 
                 foreach my $job (@{$data->{'jobs'}}) {
                     $write_lat_sum += $job->{'write'}->{'lat_ns'}->{'mean'};
-                    $write_speed_sum += $job->{'write'}->{'io_bytes'} / $job->{'write'}->{'runtime'} / 1024 / 1024 * 1000;
+                    $write_speed_sum += $job->{'write'}->{'bw_mean'} / 1024;
                 }
             }
 
@@ -83,6 +88,12 @@ elsif ($task eq 'run') {
 
     elsif ($experiment eq 'rw_cent') {
         my $num_runs = 3;  # Number of iterations to run each test
+
+        # Use input number of runs if specified
+        if(@ARGV == 3) {
+            $num_runs = $input_runs;
+        }
+
         @x_data = (.0, .25, .50, .75, 1.0);
         $graphCMD = 'combine_both';
         $x_column = 'Read Percentage';
@@ -99,7 +110,7 @@ elsif ($task eq 'run') {
 
                 foreach my $job (@{$data->{'jobs'}}) {
                     $read_lat_sum += $job->{'read'}->{'lat_ns'}->{'mean'} * $read_cent;
-                    $read_speed_sum += $job->{'read'}->{'io_bytes'} / $job->{'read'}->{'runtime'} / 1024 / 1024 * 1000 * $read_cent;
+                    $read_speed_sum += $job->{'read'}->{'bw_mean'} / 1024 * $read_cent;
                 }
 
                 # Run write test
@@ -108,7 +119,7 @@ elsif ($task eq 'run') {
 
                 foreach my $job (@{$data->{'jobs'}}) {
                     $write_lat_sum += $job->{'write'}->{'lat_ns'}->{'mean'} * $write_cent;
-                    $write_speed_sum += $job->{'write'}->{'io_bytes'} / $job->{'write'}->{'runtime'} / 1024 / 1024 * 1000 * $write_cent;
+                    $write_speed_sum += $job->{'write'}->{'bw_mean'} / 1024 * $write_cent;
                 }
             }
 
@@ -121,7 +132,13 @@ elsif ($task eq 'run') {
     }
 
     elsif ($experiment eq 'queue_depth') {
-        my $num_runs = 8    ;  # Number of iterations to run each test
+        my $num_runs = 8;  # Number of iterations to run each test
+
+        # Use input number of runs if specified
+        if(@ARGV == 3) {
+            $num_runs = $input_runs;
+        }
+
         @x_data = (1, 2, 4, 8, 16, 64, 128);
         $graphCMD = 'combine_both';
         $x_column = "Queue Depth";
@@ -136,7 +153,7 @@ elsif ($task eq 'run') {
 
                 foreach my $job (@{$data->{'jobs'}}) {
                     $read_lat_sum += $job->{'read'}->{'lat_ns'}->{'mean'} * 0.5;
-                    $read_speed_sum += $job->{'read'}->{'io_bytes'} / $job->{'read'}->{'runtime'} / 1024 / 1024 * 1000 * 0.5;
+                    $read_speed_sum += $job->{'read'}->{'bw_mean'} / 1024 * 0.5;
                 }
 
                 # Run write test
@@ -145,7 +162,7 @@ elsif ($task eq 'run') {
 
                 foreach my $job (@{$data->{'jobs'}}) {
                     $write_lat_sum += $job->{'write'}->{'lat_ns'}->{'mean'} * 0.5;
-                    $write_speed_sum += $job->{'write'}->{'io_bytes'} / $job->{'write'}->{'runtime'} / 1024 / 1024 * 1000 * 0.5;
+                    $write_speed_sum += $job->{'write'}->{'bw_mean'} / 1024 * 0.5;
                 }
             }
 
@@ -173,7 +190,9 @@ elsif ($task eq 'run') {
 
     print "Data exported to data.csv\n";
 
+    # Edit for your system
     system("python3 graph.py $graphCMD");
+    # End edit
 }
 
 elsif ($task eq 'clean') {
