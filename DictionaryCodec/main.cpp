3645e7bfaf0a7ef81c632c66e68a6aa2004406b6
AshtonRopp <ashton.ropp@gmail.com>
2024-11-09 21:44:51 -0500

    Added support for loading from saved data file

--- DictionaryCodec/main.cpp
+++ DictionaryCodec/main.cpp
@@ -1,13 +1,16 @@
+// Main.cpp
 #include "Codec.h"
 #include <iostream>
-#include <string.h>  // strcmp 
-
+#include <string.h>  // strcmp
+#include <random>
 
 int main(int argc, char* argv[]) {
     if (argc == 1) {
         std::cout << "Error: please refer to README.md for correct usage." << std::endl;
         return 0;
     }
+
+    std::random_device rd;
 
     if (strcmp(argv[1], "write_encoding") == 0) {
         DictionaryCodec dict;
@@ -17,6 +20,15 @@ int main(int argc, char* argv[]) {
 
     else if (strcmp(argv[1], "query_items") == 0) {
         DictionaryCodec dict;
+
+        dict.LoadEncodedFile("src/Output.txt");
+
+        size_t maxIndex = dict.GetDataSize();
+
+        for(int i = 0; i < 10000; i++) {
+            std::cout << "Querying" << std::endl;
+            dict.QueryItem(dict.GetData(rd()%maxIndex));
+        }
 
     }
 
@@ -29,4 +41,4 @@ int main(int argc, char* argv[]) {
     }
 
     return 0;
-} 
+}
